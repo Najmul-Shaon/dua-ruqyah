bf82612221736f1d2813d2832ef0aa9354cb0e09
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-04-16 23:55:43 +0600

    project setup

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h3 className="text-2xl bg-amber-400">Hello</h3>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
+  );
+}
